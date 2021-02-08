45b45ec05ae7d73c10773a468e48974ca2a66e4c
Tomomi <58757939+tomi-design@users.noreply.github.com>
2021-02-08 02:05:20 -0500

    Added links to css and JS files in HTML and added variables in JS

---
+++ script.js
@@ -0,0 +1,6 @@
+const itemText = document.getElementById('item-text');
+const submitButton = document.getElementById('submit-button');
+const listItem = document.querySelector('.item-container');
+const clearButton = document.getElementById('clear');
+
+
