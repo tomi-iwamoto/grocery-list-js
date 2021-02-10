d296d2dfb69c533efb17b784436c37880dd4cfac
Tomomi <58757939+tomi-design@users.noreply.github.com>
2021-02-09 23:31:19 -0500

    Added an event listener to have a hover effect with the pointer on each item listed

--- script.js
+++ script.js
@@ -3,29 +3,40 @@ const clearButton = document.getElementById('clear');
 const removeMark = document.querySelector('.remove');
 
 
-//After I type in an item in the input field, I click the Add button
-//Once the Add button is clicked, the item I typed in will show up in the "item-container" (which is the variable "listItem")
-//I can remove each item by clicking on the "x" of the same line
-//When I click on the clear button, it will clear all items I typed 
+
 
 submitButton.addEventListener('click', function() {
 
     const itemContainer = document.getElementById('item-container');
-    const itemText = document.getElementById('item-text');
+    var itemText = document.getElementById('item-text');
+    //This is the content of whatever is typed into the input field
+    var item = itemText.value;
 
     //When the submit button is clicked, this function will create a new div element. And it will attach itself to the div with the id of "item-container"
-    let div = document.createElement('div');
-    itemContainer.appendChild(div);
+    var div = document.createElement('div');
+    itemContainer.append(div);
     div.classList.add('show');
-
-    //This is the content of whatever is typed into the input field
-    let item = itemText.value;
 
     //Whatever was typed into the input field will be the text of the new div
     div.innerText = item;
 
     //After the submit button is clicked, the input field becomes empty again.
     itemText.value = ' ';
+
+
+
+    //This event listener will put a strike through style into each div element when clicked on
+    div.addEventListener('click', function () {
+
+        div.style.textDecoration = "line-through";
+
+    })
+
+    //Added an event listener that hovers over each created div with a pointer
+    div.addEventListener('mouseover', function () {
+
+        div.style.cursor = "pointer";
+    })
 
 })
 
